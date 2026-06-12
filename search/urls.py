9d084628f26fb6c5136aa4638f61205da2ba3fd5
AstralOversight <Paul.Marmet@ssc-spc.gc.ca>
2026-06-12 19:34:07 +0000

    feat: Not quite functional search UI.
    I said it's not functional. And also some of the boxes are acting weird. But like, It's something.

--- search/urls.py
+++ search/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.page, name="search"),
+    path("", views.page, name="search-base"),
+    path("?{{ request.GET.urlencode }}", views.page, name="search"),
 ]
