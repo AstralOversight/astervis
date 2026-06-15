cac2b76fe5f628c34cb2e261380d1ad4bf2d0222
AstralOversight <Paul.Marmet@ssc-spc.gc.ca>
2026-06-15 18:30:55 +0000

    feat: The actual observation data is there now.
    No more temp "String" and "Number" and "Date"
    ALSO favicon.ico, might make it an SVG eventually but this will do for now.

--- search/views.py
+++ search/views.py
@@ -3,11 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from visualizer import models
+from django.db.models import fields
 
 
 def index(request):
     return HttpResponse("Hello, you are at the search index.")
 
 def page(request):
-    context = {"observation_list": models.ObservationSet.objects.order_by("-obs_id")[:50]}
+    stringed = ""
+    for field in models.ObservationSet._meta.get_fields():
+        type = field
+        match field.__class__:
+            case fields.IntegerField | fields.FloatField | fields.BigAutoField:
+                type = "n"
+            case fields.DateTimeField:
+                type = "d"
+            case fields.BooleanField:
+                type = "b"
+            case fields.CharField | fields.TextField | _:
+                type = "s"
+        stringed += "," + type + field.attname
+    context = {"observation_list": models.ObservationSet.objects.order_by("-obs_id")[:50],
+               "fields": stringed}
     return render(request, "search/search.html", context)
