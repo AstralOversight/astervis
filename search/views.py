774ebcb07f5a55bbec785847f7b372dd2e60c3a7
AstralOversight <Paul.Marmet@ssc-spc.gc.ca>
2026-06-18 12:39:36 +0000

    feat: Somewhat functional search and histogram.
    Can now find by more than just an exact match, if you ignore the strings...
    Red bars to indicate where the black and white values are located.

--- search/views.py
+++ search/views.py
@@ -6,6 +6,65 @@ from visualizer.models import ObservationSet
 from django.db.models import fields
 import datetime
 
+searches = [
+    # Types
+    # String
+    [
+        # Operation - Figure out how to add the %
+        # Match
+        "{param} LIKE '{value}'",
+        # Contains
+        "{param} LIKE '{value}'",
+        # Greater
+        "{param} LIKE '{value}'",
+        # Lesser
+        "{param} LIKE '{value}'",
+        # Exclude
+        "NOT ({param} LIKE '{value}')",
+    ],
+    # Number
+    [
+        # Operation
+        # Match
+        "{param} = {value}",
+        # Contains
+        "{param} = {value}",
+        # Greater
+        "{param} > {value}",
+        # Lesser
+        "{param} < {value}",
+        # Exclude
+        "NOT ({param} = {value})",
+    ],
+    # Date
+    [
+        # Operation
+        # Match
+        "{param} = '{value}'",
+        # Contains
+        "{param} = '{value}'",
+        # Greater
+        "{param} > '{value}'",
+        # Lesser
+        "{param} < '{value}'",
+        # Exclude
+        "NOT ({param} = '{value}')",
+    ],
+    # Bool
+    [
+        # Operation
+        # Match
+        "{param} IS {value}",
+        # Contains
+        "{param} IS {value}",
+        # Greater
+        "{param} IS {value}",
+        # Lesser
+        "{param} IS {value}",
+        # Exclude
+        "NOT ({param} IS {value})",
+    ],
+]
 
 def index(request):
     return HttpResponse("Hello, you are at the search index.")
@@ -30,17 +89,35 @@ def page(request):
         if search: 
             search += " AND "
         
+        type = 0
+        value = request.GET[param][1:]
         field = ObservationSet._meta.get_field(param)
         match field.__class__:
             case fields.IntegerField | fields.FloatField | fields.BigAutoField:
-                search += param + " = " + request.GET[param]
+                type = 1
             case fields.DateTimeField:
-                search += param + " = '" + " ".join(request.GET[param].split("T")) + "'"
+                type = 2
+                value = " ".join(request.GET[param][1:].split("T"))
             case fields.BooleanField:
-                val = "True" if request.GET[param] == "on" else "False"
-                search += param + " IS " + val
+                type = 3
+                value = "True" if request.GET[param][1:] == "on" else "False"
             case fields.CharField | fields.TextField | _:
-                search += param + " LIKE '" + request.GET[param] + "'"
+                type = 0
+        
+        comp = 0
+        match request.GET[param][0]:
+            case "c":
+                comp = 1
+            case "g":
+                comp = 2
+            case "l":
+                comp = 3
+            case "e":
+                comp = 4
+            case "m" | _:
+                comp = 0
+        
+        search += searches[type][comp].format(param=param, value=value)
     
     sql = "SELECT id, obs_id FROM visualizer_observationset"
     if search:
