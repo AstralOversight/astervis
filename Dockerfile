e6e9aeed334a65a81bc92d23d1ef52e5eafad0e0
AstralOversight <Paul.Marmet@ssc-spc.gc.ca>
2026-06-23 18:58:18 +0000

    fix: yeah, as expected, that didn't seem to fix it. This probably won't either, but what if? like, in theory the migration has taken place by now?

--- Dockerfile
+++ Dockerfile
@@ -16,5 +16,6 @@ RUN python -m pip install -r requirements.txt
 # Run the thing
 COPY . .
 EXPOSE 80
-CMD python ./manage.py migrate --noinput && gunicorn astervis.wsgi:application
+CMD gunicorn astervis.wsgi:application
+# CMD python ./manage.py migrate --noinput && gunicorn astervis.wsgi:application
 # ["python", "./manage.py", "runserver", "0.0.0.0:80"]
--- astervis/settings.py
+++ astervis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DJANGO_DEBUG", default=0))
 
-ALLOWED_HOSTS = ["poc.fsdh-dhsf.science.cloud-nuage.canada.ca", "fsdh-proj-fewpm-webapp-poc.azurewebsites.net"] + os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
