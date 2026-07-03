769856888ea1204f9474a3501b2f4b452d94ae10
AstralOversight <Paul.Marmet@ssc-spc.gc.ca>
2026-07-03 19:18:08 +0000

    feat: Version Swapping
    You can now change between raw, cor and cord versions of observations when possible.

--- visualizer/views.py
+++ visualizer/views.py
@@ -17,7 +17,13 @@ def set(request, obs_name):
 
 def get(request, obs_name):
     obs_set = get_object_or_404(ObservationSet, name=obs_name)
-    obs_type = getfits.ObsType.RAW
+    match request.GET["obs"]:
+        case "COR":
+            obs_type = getfits.ObsType.COR
+        case "CORD":
+            obs_type = getfits.ObsType.CORD
+        case "RAW" | _:
+            obs_type = getfits.ObsType.RAW
     content_type, encoding = guess_type(obs_set.name + obs_type)
     content_type = content_type or "application/octet-stream"
     response = FileResponse(getfits.stream_file(obs_set, obs_type), content_type=content_type)
