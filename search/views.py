a01745df8c3f213d7508f715d7aebd604c601938
AstralOversight <Paul.Marmet@ssc-spc.gc.ca>
2026-07-20 19:11:54 +0000

    fix: The operations JSON now also gets generated from the operations variable.
    Potentially also somewhat joining with the header table. Might not work super well though.

--- search/views.py
+++ search/views.py
@@ -7,106 +7,64 @@ from django.db.models import fields, ManyToOneRel
 import datetime
 
 specialTerms = ["page"]
-searches = [
-    # Types
-    # String
-    [
-        # Operation - Figure out how to add the %
-        # Match
-        "{param} LIKE '{value}'",
-        # Contains
-        "{param} LIKE '%%{value}%%'",
-        # Starts with
-        "{param} LIKE '{value}%%'",
-        # Ends with
-        "{param} LIKE '%%{value}'",
-        # Exclude
-        "NOT {param} LIKE '%%{value}%%'",
-    ],
-    # Number
-    [
-        # Operation
-        # Match
-        "{param} = {value}",
-        # Error
-        "{param} = {value}",
-        # Greater
-        "{param} > {value}",
-        # Lesser
-        "{param} < {value}",
-        # Exclude
-        "NOT {param} = {value}",
-    ],
-    # Date
-    [
-        # Operation
-        # Match
-        "{param} = '{value}'",
-        # Within 24h
-        "{param} = '{value}'",
-        # Before
-        "{param} > '{value}'",
-        # After
-        "{param} < '{value}'",
-        # Exclude
-        "NOT {param} = '{value}'",
-    ],
-    # Bool
-    [
-        # Operation
-        # Is
-        "{param} IS {value}",
-        # Error
-        "{param} IS {value}",
-        # Error
-        "{param} IS {value}",
-        # Error
-        "{param} IS {value}",
-        # Is not
-        "NOT {param} IS {value}",
-    ],
-]
 
-operationsJSON = '[{"id":"s","values":[' \
-'{"id":"m","name":"matches"},' \
-'{"id":"c","name":"contains"},' \
-'{"id":"l","name":"starts with"},' \
-'{"id":"g","name":"ends with"},' \
-'{"id":"e","name":"excludes"}]},' \
-'{"id":"n","values":[' \
-'{"id":"m","name":"matches"},' \
-'{"id":"l","name":"lesser than"},' \
-'{"id":"g","name":"greater than"},' \
-'{"id":"e","name":"not"}]},' \
-'{"id":"d","values":[' \
-'{"id":"m","name":"matches"},' \
-'{"id":"c","name":"within 24h"},' \
-'{"id":"l","name":"before"},' \
-'{"id":"g","name":"after"},' \
-'{"id":"e","name":"is not"}]},' \
-'{"id":"b","values":[' \
-'{"id":"m","name":"is"},' \
-'{"id":"e","name":"is not"}]}]'
+def type_code_for_class(field) -> str:
+    type_code = "s"
+    match field.__class__:
+        case fields.CharField | fields.TextField:
+            type_code = "s"
+        case fields.IntegerField | fields.FloatField | fields.BigAutoField | fields.BigIntegerField | fields.related.ForeignKey:
+            type_code = "n"
+        case fields.DateTimeField:
+            type_code = "d"
+        case fields.BooleanField:
+            type_code = "b"
+    return type_code
 
-groups = {"set":[ObservationSet,"Set"],
-           "header":[ObsHeader,"Header"]}
+operations = {
+    "s": {
+        "m": ["matches", "{param} LIKE '{value}'"],
+        "c": ["contains", "{param} LIKE '%%{value}%%'"],
+        "l": ["starts with", "{param} LIKE '{value}%%'"],
+        "g": ["ends with", "{param} LIKE '%%{value}'"],
+        "e": ["does not contain", "NOT {param} LIKE '%%{value}%%'"],
+    },
+    "n": {
+        "m": ["matches", "{param} = {value}"],
+        "g": ["greater than", "{param} > {value}"],
+        "l": ["lesser than", "{param} < {value}"],
+        "n": ["is not", "NOT {param} = {value}"],
+    },
+    "d": {
+        "m": ["matches", "{param} = '{value}'"],
+        # within 24h
+        "b": ["before", "{param} < '{value}'"],
+        "a": ["after", "{param} > '{value}'"],
+        "n": ["is not", "NOT {param} = '{value}'"],
+    },
+    "b": {
+        "i": ["is", "{param} IS {value}"],
+        "n": ["is not", "NOT {param} IS {value}"],
+    },
+}
+operationsJSON = '['
+for var_type in operations:
+    operationsJSON += '{"id":"'+var_type+'","values":['
+    for operation_id in operations[var_type]:
+        operationsJSON += '{"id":"'+operation_id+'","name":"'+operations[var_type][operation_id][0]+'"},'
+    operationsJSON = operationsJSON[:-1] + ']},'
+operationsJSON = operationsJSON[:-1] + ']'
 
+groups = {
+    "set":[ObservationSet,"Set"],
+    "header":[ObsHeader,"Header"],
+}
 fieldsJSON = '['
 for group in groups:
     fieldsJSON += '{"id":"'+group+'","name":"'+groups[group][1]+'","values":['
     for field in groups[group][0]._meta.get_fields():
         if (not isinstance(field, ManyToOneRel)):
-            field_type = field
-            match field.__class__:
-                case fields.IntegerField | fields.FloatField | fields.BigAutoField | fields.BigIntegerField | fields.related.ForeignKey:
-                    field_type = "n"
-                case fields.DateTimeField:
-                    field_type = "d"
-                case fields.BooleanField:
-                    field_type = "b"
-                case fields.CharField | fields.TextField | _:
-                    field_type = "s"
-            fieldsJSON += '{"id":"'+field.attname+'","type":"'+field_type+'"},'
+            fieldsJSON += '{"id":"'+field.attname+'","type":"'+type_code_for_class(field)+'"},'
     fieldsJSON = fieldsJSON[:-1] + ']},'
 fieldsJSON = fieldsJSON[:-1] + ']'
 
@@ -120,41 +78,25 @@ def page(request):
             if search: 
                 search += " AND "
             
-            search_type = 0
-            value = request.GET[param][1:]
-            cval = request.GET[param][0]
             group = param.split('.')[0]
             field = param.split('.')[1]
-            match groups[group][0]._meta.get_field(field).__class__:
-                case fields.IntegerField | fields.FloatField | fields.BigAutoField | fields.BigIntegerField | fields.related.ForeignKey:
-                    search_type = 1
-                case fields.DateTimeField:
-                    search_type = 2
+
+            op = request.GET[param][0]
+            value = request.GET[param][1:]
+
+            type_code = type_code_for_class(groups[group][0]._meta.get_field(field))
+            match type_code:
+                case "d":
                     value = " ".join(value.split("T"))
-                case fields.BooleanField:
-                    search_type = 3
+                case "b":
                     value = "True" if (value == "on" or value == "true") else "False"
-                case fields.CharField | fields.TextField | _:
-                    search_type = 0
             
-            comp = 0
-            match cval:
-                case "c":
-                    comp = 1
-                case "g":
-                    comp = 2
-                case "l":
-                    comp = 3
-                case "e":
-                    comp = 4
-                case "m" | _:
-                    comp = 0
-            
-            search += searches[search_type][comp].format(param=param, value=value)
+            search += operations[type_code][op][1].format(param=param, value=value)
     
-    sql = "SELECT id, name FROM visualizer_observationset"
+    sql = "SELECT id, name FROM visualizer_observationset as set NATURAL JOIN visualizer_obsheader as header"
     if search:
-        sql += f" WHERE {search} ORDER BY dt ASC"
+        sql += f" WHERE {search}"
+    sql += " ORDER BY dt ASC"
 
     obss = ObservationSet.objects.raw(sql)
 
