f2b4b8809cf1252b878c71b4f13f6c41fee7de06
AstralOversight <Paul.Marmet@ssc-spc.gc.ca>
2026-07-22 19:50:36 +0000

    fix: More resistant to SQL injections.
    If you knew how to do it before, you totally could just do that.
    But not anymore.
    I think.

--- search/views.py
+++ search/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from visualizer.models import ObservationSet, ObsHeader
-from django.db.models import fields, ManyToOneRel
+from django.db.models import fields, ManyToOneRel, Q
 import datetime
 
 specialTerms = ["page"]
@@ -23,28 +23,28 @@ def type_code_for_class(field) -> str:
 
 operations = {
     "s": {
-        "m": ["matches", "{param} LIKE '{value}'"],
-        "c": ["contains", "{param} LIKE '%%{value}%%'"],
-        "l": ["starts with", "{param} LIKE '{value}%%'"],
-        "g": ["ends with", "{param} LIKE '%%{value}'"],
-        "e": ["does not contain", "NOT {param} LIKE '%%{value}%%'"],
+        "m": ["matches", "iexact", False],
+        "c": ["contains", "contains", False],
+        "l": ["starts with", "startswith", False],
+        "g": ["ends with", "endswith", False],
+        "e": ["does not contain", "exact", True],
     },
     "n": {
-        "m": ["matches", "{param} = {value}"],
-        "g": ["greater than", "{param} > {value}"],
-        "l": ["lesser than", "{param} < {value}"],
-        "n": ["is not", "NOT {param} = {value}"],
+        "m": ["matches", "exact", False],
+        "g": ["greater than", "gt", False],
+        "l": ["lesser than", "lt", False],
+        "n": ["is not", "exact", True],
     },
     "d": {
-        "m": ["matches", "{param} = '{value}'"],
+        "m": ["matches", "exact", False],
         # within 24h
-        "b": ["before", "{param} < '{value}'"],
-        "a": ["after", "{param} > '{value}'"],
-        "n": ["is not", "NOT {param} = '{value}'"],
+        "b": ["before", "lt", False],
+        "a": ["after", "gt", False],
+        "n": ["is not", "exact", True],
     },
     "b": {
-        "i": ["is", "{param} IS {value}"],
-        "n": ["is not", "NOT {param} IS {value}"],
+        "i": ["is", "exact", False],
+        "n": ["is not", "exact", True],
     },
 }
 operationsJSON = '['
@@ -72,39 +72,46 @@ def index(request):
     return HttpResponse("Hello, you are at the search index.")
 
 def page(request):
-    search = ""
-    has_header = False
+    # search = ""
+    # has_header = False
+    filters = Q()
     for param in request.GET:
         if param not in specialTerms:
-            if search: 
-                search += " AND "
+            # if search: 
+                # search += " AND "
             
             group = param.split('.')[0]
             field = param.split('.')[1]
 
-            if (not has_header and group == "header") :
-                has_header = True
+            key = field
+            if (not group == "set") :
+                key = group + "__" + key
 
             op = request.GET[param][0]
             value = request.GET[param][1:]
 
             type_code = type_code_for_class(groups[group][0]._meta.get_field(field))
+            key += "__" + operations[type_code][op][1]
             match type_code:
                 case "d":
                     value = " ".join(value.split("T"))
                 case "b":
-                    value = "True" if (value == "on" or value == "true") else "False"
-            
-            search += operations[type_code][op][1].format(param=param, value=value)
-    
-    sql = "SELECT set.id, set.name FROM visualizer_observationset AS set"
-    if search:
-        if has_header:
-            sql += " INNER JOIN visualizer_obsheader AS header ON set.header_id = header.id"
-        sql += f" WHERE {search}"
-    sql += " ORDER BY dt ASC;"
+                    value = True if (value == "on" or value == "true") else False
 
-    obss = ObservationSet.objects.raw(sql)
+            if (operations[type_code][op][2]):
+                filters = filters & ~Q(**{key: value})
+            else:
+                filters = filters & Q(**{key: value})
+            # search += operations[type_code][op][1].format(param=param, value=value)
+    
+    # sql = "SELECT set.id, set.name FROM visualizer_observationset AS set"
+    # if search:
+    #     if has_header:
+    #         sql += " INNER JOIN visualizer_obsheader AS header ON set.header_id = header.id"
+    #     sql += f" WHERE {search}"
+    # sql += " ORDER BY dt ASC;"
+
+    obss = ObservationSet.objects.filter(filters).order_by("dt")
 
     # What page to display
     pageN = 1
